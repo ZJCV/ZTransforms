4ff68702264129e2e85c38c2ae9305ea7a2e86e0
zjkjzj <wy163zhuj@163.com>
2021-01-12 14:32:32 +0800

    feat(transforms): 添加torchvision.transforms实现

---
+++ ztransforms/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/1/12 下午2:23
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
+
+import torchvision.transforms as transforms
+
+from .ztransforms import *
